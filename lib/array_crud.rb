9b14cf73bc5fe4ff741209bdf36cb0ef566a7336
Jesus <jose.jesus16@gmail.com>
2021-03-07 02:25:55 -0500

    commit tree 8ce3669ae53987589a080e8202e84faed143a95d

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,43 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+ artists = ["paul","peter","john","joseph"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  heroes = ["superman", "batman","johny quest"]
+  heroes.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    heroes = ["superman", "batman","johny quest"]
+    heroes.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+    heroes = ["superman", "batman","johny quest","arrays!"]
+    arrays = heroes.pop
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+   heroes = ["wow","superman", "batman","johny quest","arrays!"]
+  wow = heroes.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    heroes = ["wow","superman","am", "batman","johny quest","arrays!"]
+    heroes[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    heroes = ["wow","superman","am", "batman","johny quest","arrays!"]
+    heroes[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    heroes = ["wow","superman","am", "batman","johny quest","arrays!"]
+    heroes[5]
 end
